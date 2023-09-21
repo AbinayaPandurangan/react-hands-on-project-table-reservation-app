061876106427f30a5c11ba31e0f638b188fb2b53
Abinaya Pandurangan <abi1243@gmail.com>
2023-09-21 11:58:01 +0200

    Update validateEmail.js

--- src/components/pages/bookings/utils/validateEmail.js
+++ src/components/pages/bookings/utils/validateEmail.js
@@ -1,11 +1,3 @@
-// export const validateEmail = (email) => {
-//   String(email)
-//     .toLowerCase()
-//     .match(
-//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//     );
-// };
-
 function isEmailValid(email) {
   if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
     return true;
