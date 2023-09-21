b119918321aa5d0613500604aaec7d4991d3cac2
Abinaya Pandurangan <abi1243@gmail.com>
2023-09-21 16:36:53 +0200

    added error page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BookingPage from "./components/pages/bookings/ReservationPage";
 import ConfirmationPage from "./components/pages/bookings/ConfirmationPage";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Error from "./components/pages/error";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           path="/booking/confirmationpage"
           element={<ConfirmationPage />}
         ></Route>
+        <Route path="/error" element={<Error />}></Route>
       </Routes>
 
       <Footer />
